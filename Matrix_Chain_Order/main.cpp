e1f2fc10e61bbbdba0c567915179fb7378689e00
YanzheL <lee.yanzhe@yanzhe.org>
2017-07-15 07:56:08 +0800

    Initial Commit

---
+++ Matrix_Chain_Order/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Matrix_Chain_Order
+//
+//  Created by LI YANZHE on 15/07/2017.
+//  Copyright © 2017 Yanzhe Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
